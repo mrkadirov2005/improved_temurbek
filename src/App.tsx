89795016069afeeadff5c2cea54bd59ef7c3a315
mrkadirov2005 <muzaffar571181@gmail.com>
2025-07-25 06:58:25 +0500

    submitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import HeaderS from './components/header/header'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route,  Routes } from 'react-router-dom'
 import Main from './pages/Main'
 import CourseComponent from './pages/Courses/page'
 import Footer from './components/footer/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className='flex  flex-col '>
--- src/components/achievements/achievements.tsx
+++ src/components/achievements/achievements.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FaUserFriends, FaGift, FaMedal, FaBullhorn } from 'react-icons/fa';
 
 const achievements = [
